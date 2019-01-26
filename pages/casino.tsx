7fcd30598050167c0123706a6aa25ce87b09a393
Sygeman <sygeman@gmail.com>
2019-01-26 07:52:51 +0300

    fix layout for casino

--- pages/casino.tsx
+++ pages/casino.tsx
@@ -1,6 +1,11 @@
 import { FC } from 'react';
 import Casino from '../components/Casino';
+import Layout from '../layouts/Main';
 
-const CasinoPage: FC = () => <Casino />;
+const CasinoPage: FC = () => (
+  <Layout>
+    <Casino />
+  </Layout>
+);
 
 export default CasinoPage;

486de5f65c6cb43f7fce0925a1e31175a647561b
Sygeman <sygeman@gmail.com>
2019-01-26 07:53:46 +0300

    new tag page

--- pages/tag.tsx
+++ pages/tag.tsx
@@ -3,11 +3,9 @@ import Head from 'next/head';
 import { RouterProps, withRouter } from 'next/router';
 import * as React from 'react';
 import { Query } from 'react-apollo';
-import RightPanel from '../components/Nav/Right';
 import Posts from '../components/Posts';
-import Streams from '../components/Streams';
+import Streams from '../components/Stream';
 import Layout from '../layouts/Main';
-import styled from '../theme';
 
 const GET_TAG = gql`
   query tag($id: ID!) {
@@ -16,21 +14,6 @@ const GET_TAG = gql`
       title
     }
   }
-`;
-
-const Box = styled.div`
-  display: flex;
-  justify-content: center;
-  margin: 0 auto;
-  padding: 20px 0;
-`;
-
-const PostsBox = styled.div`
-  margin: 0 20px;
-  display: flex;
-  flex: 1;
-  border-radius: 5px;
-  overflow: hidden;
 `;
 
 interface IProps {
@@ -53,39 +36,27 @@ class UserPage extends React.Component<IProps> {
 
     return (
       <Layout>
-        <Box>
-          <Query query={GET_TAG} variables={{ id: tagId }}>
-            {({ loading, error, data }) => {
-              if (loading) {
-                return null;
-              }
+        <Query query={GET_TAG} variables={{ id: tagId }}>
+          {({ loading, error, data }) => {
+            if (loading) {
+              return null;
+            }
 
-              if (error || !data || !data.tag) {
-                return null;
-              }
+            if (error || !data || !data.tag) {
+              return null;
+            }
 
-              return (
-                <>
-                  <Head>
-                    <title>#{data.tag.title}</title>
-                  </Head>
-                  <PostsBox>
-                    <Posts
-                      title={`#${data.tag.title}`}
-                      tagId={tagId}
-                      sort="new"
-                    />
-                  </PostsBox>
-                  <RightPanel.Box>
-                    <RightPanel.Block>
-                      <Streams />
-                    </RightPanel.Block>
-                  </RightPanel.Box>
-                </>
-              );
-            }}
-          </Query>
-        </Box>
+            return (
+              <>
+                <Head>
+                  <title>#{data.tag.title}</title>
+                </Head>
+                <Streams />
+                <Posts title={`#${data.tag.title}`} tagId={tagId} sort="new" />
+              </>
+            );
+          }}
+        </Query>
       </Layout>
     );
   }

316d9c72db8cbf2689cad3b1dd9868e3b2528c9e
Sygeman <sygeman@gmail.com>
2019-02-02 23:28:43 +0300

    VideoPreview fix

--- ui/VideoPreview/VideoPreview.tsx
+++ ui/VideoPreview/VideoPreview.tsx
@@ -141,13 +141,13 @@ export class VideoPreview extends Component<IProps, IState> {
     super(props);
 
     this.state = {
-      width: 0
+      width: 300
     };
   }
 
   public componentDidMount() {
-    this.setState({ width: this.box.current.clientWidth });
-    console.log(this.box.current.clientWidth);
+    // this.setState({ width: this.box.current.clientWidth });
+    // console.log(this.box.current.clientWidth);
   }
 
   public render() {

03dd3cc6ffdcbcc95193da41c34a4f8c9fd8e698
Sygeman <sygeman@gmail.com>
2019-02-06 23:22:47 +0300

    [fix] hooks: useMeasure

--- hooks/useMeasure.tsx
+++ hooks/useMeasure.tsx
@@ -1,12 +1,20 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import ResizeObserver from 'resize-observer-polyfill';
 
-export default function useMeasure() {
-  const ref = useRef(null);
+export default function useMeasure(ref) {
   const [bounds, set] = useState({ left: 0, top: 0, width: 0, height: 0 });
   const [ro] = useState(
     () => new ResizeObserver(([entry]) => set(entry.contentRect))
   );
-  useEffect(() => (ro.observe(ref.current), ro.disconnect), []);
-  return [{ ref }, bounds];
+  useEffect(() => {
+    if (!ref.current) {
+      return null;
+    }
+
+    return () => {
+      ro.observe(ref.current);
+      ro.disconnect();
+    };
+  }, []);
+  return [bounds];
 }

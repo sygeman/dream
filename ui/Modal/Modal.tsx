79a00a4f30cd001b37e34d6bd4ffb68ec75e698b
Sygeman <sygeman@gmail.com>
2019-04-02 21:50:48 +0300

    new Modal

--- ui/Modal/Modal.tsx
+++ ui/Modal/Modal.tsx
@@ -1,5 +1,5 @@
 import nanoid from 'nanoid';
-import { lighten, rgba } from 'polished';
+import { darken, lighten, rgba } from 'polished';
 import { FC, useEffect } from 'react';
 import styled from 'styled-components';
 import { Icon } from '../Icon';
@@ -22,16 +22,21 @@ const BGOut = styled.div`
   width: 100%;
   top: 0;
   left: 0;
-  background: ${({ theme }) => theme.dark1Color};
-  opacity: 0.7;
+  background: ${({ theme }) => darken(0.05, theme.dark1Color)};
+  opacity: 0.9;
   z-index: 3000;
 `;
 
 const BoxW = styled.div`
   z-index: 3500;
   margin: auto;
-  padding: 60px 0;
+  /* padding: 50px 0; */
   display: flex;
+
+  @media (max-width: 700px) {
+    width: 100%;
+    height: 100%;
+  }
 `;
 
 const BoxNav = styled.div`
@@ -50,6 +55,10 @@ const BoxNav = styled.div`
   :hover {
     color: ${({ theme }) => theme.accent2Color};
   }
+
+  @media (max-width: 700px) {
+    display: none;
+  }
 `;
 
 const Box = styled('div')<{
@@ -58,7 +67,7 @@ const Box = styled('div')<{
   min-width: 240px;
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
   margin: auto;
-  padding: ${({ minimal }) => (minimal ? '0' : '20px')};
+  padding: 10px;
   z-index: 3500;
   display: flex;
   position: relative;
@@ -68,14 +77,23 @@ const ModalB = styled.div`
   background: ${({ theme }) =>
     theme.dark2Color && lighten(0.01, theme.dark2Color)};
   border-radius: 4px;
+  overflow: hidden;
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
 `;
 
-const Header = styled.div`
-  display: flex;
+const Header = styled('div')<{
+  minimal: boolean;
+}>`
+  display: ${({ minimal }) => (minimal ? 'none' : 'flex')};
   align-items: center;
-  height: 50px;
-  background: ${({ theme }) => theme.main1Color};
+  height: 46px;
+  background: ${({ theme }) => darken(0.1, theme.main1Color)};
   border-radius: 4px 4px 0 0;
+
+  @media (max-width: 700px) {
+    border-radius: 0;
+    display: flex;
+  }
 `;
 
 const Title = styled.div`
@@ -92,9 +110,15 @@ const Close = styled.div`
   font-size: 22px;
   color: ${({ theme }) => theme.text1Color};
   cursor: pointer;
+  color: ${({ theme }) => lighten(0.3, theme.main1Color)};
 
   :hover {
     color: ${({ theme }) => theme.text1Color};
+  }
+
+  @media (max-width: 700px) {
+    top: 0;
+    right: 0;
   }
 `;
 
@@ -107,12 +131,16 @@ const CloseOut = styled.div`
   margin-left: auto;
   width: 60px;
   justify-content: center;
-  font-size: 34px;
+  font-size: 30px;
   color: ${({ theme }) => theme.accent2Color && rgba(theme.accent2Color, 0.5)};
   cursor: pointer;
 
   :hover {
     color: ${({ theme }) => theme.accent2Color};
+  }
+
+  @media (max-width: 700px) {
+    display: none;
   }
 `;
 
@@ -169,14 +197,12 @@ export const Modal: FC<IModalProps> = ({
               </BoxNav>
             )}
             <ModalB>
-              {!minimal && (
-                <Header>
-                  <Title>{title}</Title>
-                  <Close onClick={close}>
-                    <Icon type="close" />
-                  </Close>
-                </Header>
-              )}
+              <Header minimal={minimal}>
+                <Title>{title}</Title>
+                <Close onClick={close}>
+                  <Icon type="close" />
+                </Close>
+              </Header>
               <Content minimal={minimal}>{children}</Content>
             </ModalB>
             {minimal && (

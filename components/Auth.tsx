1888d0dc860b582d011448620059a2a6ca0b8003
Sygeman <sygeman@gmail.com>
2019-04-15 00:17:25 +0300

    Auth fix styles

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -8,20 +8,21 @@ const AuthBox = styled.div`
   display: flex;
   flex-direction: column;
   padding: 20px 40px;
+  max-width: 500px;
+  width: 500px;
 `;
 
 const AuthTitle = styled.div`
   display: flex;
   justify-content: center;
   text-align: center;
-  font-size: 0.95rem;
+  font-size: 15px;
   padding: 16px 0 20px;
   color: ${({ theme }) => theme.accent2Color};
 `;
 
 const SocialForm = styled.div`
   display: flex;
-  width: 600px;
   width: 100%;
 
   @media (max-width: 700px) {
@@ -36,7 +37,7 @@ const LoginButton = styled('a')<{
   flex: 1;
   align-items: center;
   justify-content: center;
-  height: 46px;
+  height: 44px;
   font-size: 17px;
   margin: 5px;
   text-align: center;
@@ -52,19 +53,19 @@ const LoginButton = styled('a')<{
 
   i {
     margin-bottom: 2px;
-    font-size: 22px;
+    font-size: 20px;
   }
 `;
 
 const Auth: FC = () => (
   <AuthBox>
-    <AuthTitle>Выберите наиболее удобную платформу для авторизации</AuthTitle>
+    <AuthTitle>Выберите наиболее удобную для Вас платформу</AuthTitle>
     <SocialForm>
       <LoginButton cColor={'#507299'} href={`${config.apiUrl}auth/vkontakte`}>
         <Icon type="vk" />
       </LoginButton>
       <LoginButton cColor={'#DB4437'} href={`${config.apiUrl}auth/google`}>
-        <Icon type="youtube-play" />
+        <Icon type="google" />
       </LoginButton>
       <LoginButton cColor={'#6542a6'} href={`${config.apiUrl}auth/twitch`}>
         <Icon type="twitch" />

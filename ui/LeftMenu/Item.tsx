cbc5d8b949c6bbcbbc6d1f8e415349a17d6c2545
Sygeman <sygeman@gmail.com>
2019-04-19 02:34:07 +0300

    Left menu style fix

--- ui/LeftMenu/Item.tsx
+++ ui/LeftMenu/Item.tsx
@@ -6,12 +6,14 @@ import useRouter from '../../hooks/useRouter';
 import { Icon } from '../Icon';
 
 const Item = styled.a<{ active: boolean }>`
-  border-left: 4px solid transparent;
+  margin-right: 14px;
+   border-left: 4px solid transparent;
   border-color: ${({ active, theme }) =>
-    active && darken(0.1, theme.main1Color)};
-  font-size: 14px;
+    active && darken(0.1, theme.main1Color)}; 
+  border-radius: 0 10px 10px 0;
+  font-size: 13px;
   position: relative;
-  height: 38px;
+  height: 34px;
   display: flex;
   align-items: center;
   cursor: pointer;
@@ -27,12 +29,12 @@ const Item = styled.a<{ active: boolean }>`
 
   i {
     height: 100%;
-    width: 70px;
+    width: 60px;
     display: flex;
     justify-content: center;
     align-items: center;
     left: 20px;
-    font-size: 19px;
+    font-size: 17px;
     color: ${({ theme, active }) =>
       active ? lighten(0.7, theme.accent2Color) : theme.accent2Color};
   }
--- ui/LeftMenu/SubItem.tsx
+++ ui/LeftMenu/SubItem.tsx
@@ -7,10 +7,10 @@ import useRouter from '../../hooks/useRouter';
 const SubItem = styled('a')<{
   active?: boolean;
 }>`
+  margin-right: 14px;
+  border-radius: 0 10px 10px 0;
   border-left: 4px solid transparent;
-  border-color: ${({ active, theme }) =>
-    active && darken(0.15, theme.main1Color)};
-  padding: 0 20px 0 74px;
+  padding: 0 20px 0 60px;
   height: 30px;
   display: flex;
   align-items: center;
@@ -19,12 +19,12 @@ const SubItem = styled('a')<{
   text-overflow: ellipsis;
   font-size: 13px;
   cursor: pointer;
-  color: ${({ theme, active }) => darken(active ? 0 : 0.2, theme.text1Color)};
+  color: ${({ theme, active }) => darken(active ? 0 : 0.3, theme.text1Color)};
   background: ${({ theme, active }) =>
-    active ? lighten(0.04, theme.dark1Color) : 'transparent'};
+    active ? lighten(0.05, theme.dark1Color) : 'transparent'};
 
   :hover {
-    background: ${({ theme }) => lighten(0.04, theme.dark1Color)};
+    background: ${({ theme }) => lighten(0.05, theme.dark1Color)};
   }
 
   span {

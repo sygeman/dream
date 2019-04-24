a8a78ad0eab0ba5156f90a634f187583acf19f62
Sygeman <sygeman@gmail.com>
2019-04-24 22:04:31 +0300

    [fix] FollowsProvider - new api

--- providers/Follows.tsx
+++ providers/Follows.tsx
@@ -3,85 +3,85 @@ import { Component } from 'react';
 import { Query } from 'react-apollo';
 
 const GET_USER_TWITCH_FOLLOWS = gql`
-  query userTwitchFollows($limit: Int, $offset: Int) {
-    userTwitchFollows(offset: $offset, limit: $limit) {
-      count
-      follows {
-        title
-        name
-      }
-    }
-  }
+	query twitchFollows($limit: Int, $offset: Int) {
+		twitchFollows(offset: $offset, limit: $limit) {
+			count
+			follows {
+				title
+				name
+			}
+		}
+	}
 `;
 
 interface IProps {
-  children: any;
+	children: any;
 }
 
 class Follows extends Component<IProps> {
-  public page: number;
-  public pageSize: number;
+	public page: number;
+	public pageSize: number;
 
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.page = 1;
-    this.pageSize = 50;
-  }
+		this.page = 1;
+		this.pageSize = 50;
+	}
 
-  public render() {
-    const { children } = this.props;
+	public render() {
+		const { children } = this.props;
 
-    return (
-      <Query
-        query={GET_USER_TWITCH_FOLLOWS}
-        variables={{
-          limit: this.pageSize,
-          offset: 0
-        }}
-      >
-        {({ loading, error, data, fetchMore }) => {
-          if (loading) {
-            return null;
-          }
+		return (
+			<Query
+				query={GET_USER_TWITCH_FOLLOWS}
+				variables={{
+					limit: this.pageSize,
+					offset: 0
+				}}
+			>
+				{({ loading, error, data, fetchMore }) => {
+					if (loading) {
+						return null;
+					}
 
-          if (error || !data || !data.userTwitchFollows) {
-            return null;
-          }
+					if (error || !data || !data.twitchFollows) {
+						return null;
+					}
 
-          return children({
-            follows: data.userTwitchFollows.follows,
-            hasMore: this.page * this.pageSize <= data.userTwitchFollows.count,
-            moreFollows: () => {
-              fetchMore({
-                variables: {
-                  offset: this.page * this.pageSize
-                },
-                updateQuery: (prev, { fetchMoreResult }) => {
-                  if (!fetchMoreResult) {
-                    return prev;
-                  }
+					return children({
+						follows: data.twitchFollows.follows,
+						hasMore: this.page * this.pageSize <= data.twitchFollows.count,
+						moreFollows: () => {
+							fetchMore({
+								variables: {
+									offset: this.page * this.pageSize
+								},
+								updateQuery: (prev, { fetchMoreResult }) => {
+									if (!fetchMoreResult) {
+										return prev;
+									}
 
-                  this.page++;
+									this.page++;
 
-                  return {
-                    ...prev,
-                    userTwitchFollows: {
-                      ...prev.userTwitchFollows,
-                      follows: [
-                        ...prev.userTwitchFollows.follows,
-                        ...fetchMoreResult.userTwitchFollows.follows
-                      ]
-                    }
-                  };
-                }
-              });
-            }
-          });
-        }}
-      </Query>
-    );
-  }
+									return {
+										...prev,
+										twitchFollows: {
+											...prev.twitchFollows,
+											follows: [
+												...prev.twitchFollows.follows,
+												...fetchMoreResult.twitchFollows.follows
+											]
+										}
+									};
+								}
+							});
+						}
+					});
+				}}
+			</Query>
+		);
+	}
 }
 
 export default Follows;

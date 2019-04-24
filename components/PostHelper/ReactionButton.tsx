402052016c70cac87ba11fd6c93a5b11677cd206
Sygeman <sygeman@gmail.com>
2019-04-24 21:56:12 +0300

    [rf] PostReactionButton - fix new input type

--- components/PostHelper/ReactionButton.tsx
+++ components/PostHelper/ReactionButton.tsx
@@ -9,31 +9,34 @@ import { Access } from '../../helpers/Access';
 import { Icon } from '../../ui/Icon';
 
 const SET_POST_REACTION = gql`
-  mutation setPostReaction($postId: ID!, $type: PostReaction!) {
-    setPostReaction(postId: $postId, type: $type)
-  }
+	mutation setPostReaction($postId: ID!, $type: PostReactionType!) {
+		setPostReaction(postId: $postId, type: $type)
+	}
 `;
 
 const Box = styled.div`
-  height: 100%;
-  padding: 0 16px;
-  align-items: center;
-  font-size: 14px;
-  display: flex;
+	height: 100%;
+	padding: 0 16px;
+	align-items: center;
+	font-size: 14px;
+	display: flex;
 `;
 
 interface ILikeButton {
-  active: boolean;
+	active: boolean;
 }
 
 const LikeButtonAnim = posed.div({
-  pressable: true,
-  init: { scale: 1 },
-  press: { scale: 1.5 },
-  pressEnd: { scale: 1 }
+	pressable: true,
+	init: { scale: 1 },
+	press: { scale: 1.5 },
+	pressEnd: { scale: 1 }
 });
 
-const LikeButton = styled(LikeButtonAnim)<ILikeButton>`
+const LikeButton =
+	styled(LikeButtonAnim) <
+	ILikeButton >
+	`
   display: flex;
   justify-content: center;
   padding: 5px;
@@ -42,76 +45,77 @@ const LikeButton = styled(LikeButtonAnim)<ILikeButton>`
   i {
     font-size: 21px;
     color: ${({ theme, active }) =>
-      active ? lighten(0.15, theme.main1Color) : theme.accent2Color};
+			active ? lighten(0.15, theme.main1Color) : theme.accent2Color};
   }
 `;
 
-const LikesCount = styled('div')<ILikeButton>`
+const LikesCount =
+	styled('div') <
+	ILikeButton >
+	`
   color: ${({ theme, active }) =>
-    active ? lighten(0.15, theme.main1Color) : theme.accent2Color};
+		active ? lighten(0.15, theme.main1Color) : theme.accent2Color};
   margin-left: 10px;
   font-weight: 500;
   user-select: none;
 `;
 
 interface IProps {
-  id: string;
-  icon: string;
-  type: string;
-  state: boolean;
-  count: number;
+	id: string;
+	icon: string;
+	type: string;
+	state: boolean;
+	count: number;
 }
 
 export default class PostReaction extends React.Component<IProps> {
-  public render() {
-    const { id, type, icon, state, count } = this.props;
+	public render() {
+		const { id, type, icon, state, count } = this.props;
 
-    return (
-      <Access
-        denyContent={
-          <Box
-            onClick={() =>
-              Router.push(
-                {
-                  pathname: Router.route,
-                  query: {
-                    ...Router.query,
-                    authModal: 1
-                  }
-                },
-                `/auth?continue=${Router.asPath}`,
-                { shallow: true }
-              )
-            }
-          >
-            <LikeButton active={state}>
-              <Icon type={icon} />
-            </LikeButton>
-            {count > 0 && <LikesCount active={state}>{count}</LikesCount>}
-          </Box>
-        }
-      >
-        <Mutation mutation={SET_POST_REACTION}>
-          {setPostReaction => (
-            <Box>
-              <LikeButton
-                active={state}
-                onClick={() =>
-                  setPostReaction({
-                    variables: {
-                      postId: id,
-                      type: state ? 'none' : type
-                    }
-                  })
-                }
-              >
-                <Icon type={icon} />
-              </LikeButton>
-              {count > 0 && <LikesCount active={state}>{count}</LikesCount>}
-            </Box>
-          )}
-        </Mutation>
-      </Access>
-    );
-  }
+		return (
+			<Access
+				denyContent={
+					<Box
+						onClick={() =>
+							Router.push(
+								{
+									pathname: Router.route,
+									query: {
+										...Router.query,
+										authModal: 1
+									}
+								},
+								`/auth?continue=${Router.asPath}`,
+								{ shallow: true }
+							)}
+					>
+						<LikeButton active={state}>
+							<Icon type={icon} />
+						</LikeButton>
+						{count > 0 && <LikesCount active={state}>{count}</LikesCount>}
+					</Box>
+				}
+			>
+				<Mutation mutation={SET_POST_REACTION}>
+					{(setPostReaction) => (
+						<Box>
+							<LikeButton
+								active={state}
+								onClick={() =>
+									setPostReaction({
+										variables: {
+											postId: id,
+											type: state ? 'none' : type
+										}
+									})}
+							>
+								<Icon type={icon} />
+							</LikeButton>
+							{count > 0 && <LikesCount active={state}>{count}</LikesCount>}
+						</Box>
+					)}
+				</Mutation>
+			</Access>
+		);
+	}
 }

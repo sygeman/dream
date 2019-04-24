289c829d796b9159b573b3f1eb86b51faba98d8e
Sygeman <sygeman@gmail.com>
2019-04-24 22:07:21 +0300

    [fix] Access helper - new api

--- helpers/Access.tsx
+++ helpers/Access.tsx
@@ -3,75 +3,67 @@ import { FC, ReactNode } from 'react';
 import { Query } from 'react-apollo';
 
 const GET_USER = gql`
-  query getUser {
-    user {
-      id
-      role
-      profiles {
-        id
-        name
-        avatar
-        serviceName
-        serviceId
-        visible
-      }
-    }
-  }
+	query getUser {
+		user {
+			id
+			role
+		}
+	}
 `;
 
 interface IProfile {
-  id: string;
-  name: string;
-  avatar: string;
-  serviceName: string;
-  serviceId: string;
-  visible: boolean;
+	id: string;
+	name: string;
+	avatar: string;
+	serviceName: string;
+	serviceId: string;
+	visible: boolean;
 }
 
 interface IUser {
-  id: string;
-  role: string;
-  profiles: IProfile[];
+	id: string;
+	role: string;
+	profiles: IProfile[];
 }
 
 interface IProps {
-  name?: string;
-  allow?: (currentUser: IUser) => boolean;
-  denyContent?: ReactNode;
+	name?: string;
+	allow?: (currentUser: IUser) => boolean;
+	denyContent?: ReactNode;
 }
 
 export const Access: FC<IProps> = ({ allow, children, denyContent }) => (
-  <Query query={GET_USER}>
-    {({ loading, error, data }) => {
-      if (loading) {
-        return null;
-      }
+	<Query query={GET_USER}>
+		{({ loading, error, data }) => {
+			if (loading) {
+				return null;
+			}
 
-      if (error) {
-        if (denyContent) {
-          return denyContent;
-        }
+			if (error) {
+				if (denyContent) {
+					return denyContent;
+				}
 
-        return null;
-      }
+				return null;
+			}
 
-      if (!data.user) {
-        return denyContent || null;
-      }
+			if (!data.user) {
+				return denyContent || null;
+			}
 
-      if (typeof allow === 'function' && allow(data.user)) {
-        return children;
-      }
+			if (typeof allow === 'function' && allow(data.user)) {
+				return children;
+			}
 
-      if (typeof allow !== 'function' && !!data.user) {
-        return children;
-      }
+			if (typeof allow !== 'function' && !!data.user) {
+				return children;
+			}
 
-      if (denyContent) {
-        return denyContent;
-      }
+			if (denyContent) {
+				return denyContent;
+			}
 
-      return null;
-    }}
-  </Query>
+			return null;
+		}}
+	</Query>
 );

58225b28b0c016c27e1e96c63bb53637379ef63d
Sygeman <sygeman@gmail.com>
2019-05-05 00:05:40 +0300

    [rf] PostView

--- components/Post/PostsView.tsx
+++ components/Post/PostsView.tsx
@@ -1,12 +1,9 @@
 import gql from 'graphql-tag';
-import { inject, observer } from 'mobx-react';
 import Link from 'next/link';
 import { darken } from 'polished';
-import { Component } from 'react';
+import { PureComponent } from 'react';
 import styled from 'styled-components';
-import { IStore } from '../../lib/store';
 import PostProvider from '../../providers/Post';
-import { Button } from '../../ui/Button';
 import { Grid } from '../../ui/Grid';
 import PostGridView from './GridView';
 
@@ -60,17 +57,6 @@ const PostContainer = styled.div`
   padding: 5px;
 `;
 
-const Loading = styled.div`
-  padding: 10px;
-  text-align: center;
-`;
-
-const LoadMore = styled.div`
-  padding: 10px;
-  text-align: center;
-  cursor: pointer;
-`;
-
 interface IProps {
   posts: any;
   loading: boolean;
@@ -79,50 +65,72 @@ interface IProps {
   description?: string;
   titleLink?: string;
   rows?: number;
-  store?: IStore;
   loadMore: () => Promise<any>;
   onPlay: (id: string) => void;
 }
 
-@inject('store')
-@observer
-class PostsView extends Component<IProps> {
+interface IState {
+  layoutInLoadArea: boolean;
+}
+
+interface IProcess {
+  browser: boolean;
+}
+
+declare var process: IProcess;
+
+class PostsView extends PureComponent<IProps, IState> {
   public loadLock = false;
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      layoutInLoadArea: false
+    };
+  }
+
+  scrollHandler = e => {
+    const el = e.target;
+    const offset = el.scrollHeight - el.scrollTop - el.clientHeight;
+    const layoutInLoadArea = offset <= 250;
+
+    if (this.state.layoutInLoadArea !== layoutInLoadArea) {
+      this.setState({ layoutInLoadArea });
+    }
+  };
+
+  componentDidMount() {
+    if (process.browser) {
+      document
+        .getElementById('mainScroll')
+        .addEventListener('scroll', this.scrollHandler);
+    }
+  }
+
+  componentWillUnmount() {
+    if (process.browser) {
+      document
+        .getElementById('mainScroll')
+        .removeEventListener('scroll', this.scrollHandler);
+    }
   }
 
   public componentDidUpdate() {
-    if (
-      !this.loadLock &&
-      this.props.store.layoutInLoadArea &&
-      !this.props.loading &&
-      this.props.hasMore
-    ) {
+    const { loading, hasMore, loadMore } = this.props;
+    const { layoutInLoadArea } = this.state;
+
+    if (!this.loadLock && layoutInLoadArea && !loading && hasMore) {
       this.loadLock = true;
-      this.props.loadMore().then(() => {
+
+      loadMore().then(() => {
         this.loadLock = false;
       });
     }
   }
 
   public render() {
-    const {
-      posts,
-      store,
-      loading,
-      hasMore,
-      loadMore,
-      onPlay,
-      title,
-      description,
-      rows,
-      titleLink
-    } = this.props;
-
-    /* tslint:disable */
-    store.layoutInLoadArea;
+    const { posts, onPlay, title, description, rows, titleLink } = this.props;
 
     return (
       <Grid
@@ -162,16 +170,6 @@ class PostsView extends Component<IProps> {
             </PostProvider>
           </PostContainer>
         )}
-        afterRedner={
-          <>
-            {loading && <Loading />}
-            {!loading && hasMore && (
-              <LoadMore>
-                <Button onClick={() => loadMore()}>Загрузить еще</Button>
-              </LoadMore>
-            )}
-          </>
-        }
       />
     );
   }

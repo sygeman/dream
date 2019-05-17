6457333e3ccf40641ff2182f6cc307ba39c41a8d
Sygeman <sygeman@gmail.com>
2019-05-17 17:19:47 +0300

    post page with streams block

--- pages/post.tsx
+++ pages/post.tsx
@@ -2,21 +2,44 @@ import PostFeedView from '../components/Post/FeedView';
 import useRouter from '../hooks/useRouter';
 import Layout from '../layouts/Main';
 import PostProvider from '../providers/Post';
-import styled from '../theme';
+import styled from 'styled-components';
+import { TopStreams } from '../components/TopStreams';
 
 const Box = styled.div`
-  display: flex;
-  justify-content: center;
   margin: 0 auto;
-  padding: 20px 0;
+  margin-top: 40px;
+  display: flex;
+  max-width: 1200px;
 `;
 
-const PostBox = styled.div`
-  margin: 0 20px;
-  width: 800px;
-  border-radius: 5px;
-  overflow: hidden;
+const Left = styled.div`
+  flex: 1;
+  /* max-width: 800px;
+  width: 800px; */
+  padding: 0 20px;
 `;
+
+const Right = styled.div`
+  width: 320px;
+
+  @media (max-width: 1000px) {
+    display: none;
+  }
+`;
+
+// const Box = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   margin: 0 auto;
+//   padding: 20px 0;
+// `;
+
+// const PostBox = styled.div`
+//   margin: 0 20px;
+//   width: 800px;
+//   border-radius: 5px;
+//   overflow: hidden;
+// `;
 
 const PostPage = () => {
   const router = useRouter();
@@ -29,11 +52,14 @@ const PostPage = () => {
   return (
     <Layout>
       <Box>
-        <PostBox>
+        <Left>
           <PostProvider id={postId}>
             {({ post }) => <PostFeedView {...post} header meta />}
           </PostProvider>
-        </PostBox>
+        </Left>
+        <Right>
+          <TopStreams position="column" max={3} />
+        </Right>
       </Box>
     </Layout>
   );

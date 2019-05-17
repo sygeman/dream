677685730c348ef6024afd412197321ab89eaf9b
Sygeman <sygeman@gmail.com>
2019-05-17 17:19:03 +0300

    [rf] Promoter page

--- pages/promoter.tsx
+++ pages/promoter.tsx
@@ -1,18 +1,9 @@
-import * as Settings from '../components/Settings';
 import { ChannelPromotersManage } from '../components/Settings/ChannelPromoters';
-import { Access } from '../helpers/Access';
 import Layout from '../layouts/Main';
 
 const PromoterPage = () => (
   <Layout>
-    <Settings.Box>
-      <Settings.Title>Продвижение</Settings.Title>
-      <Settings.Content>
-        <Access>
-          <ChannelPromotersManage />
-        </Access>
-      </Settings.Content>
-    </Settings.Box>
+    <ChannelPromotersManage />
   </Layout>
 );
 

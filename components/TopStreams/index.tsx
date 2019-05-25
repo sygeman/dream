7080b54266f806f411863b8914585266db3c7d58
Sygeman <sygeman@gmail.com>
2019-05-25 15:59:15 +0300

    TopStream - AddStream

--- components/TopStreams/index.tsx
+++ components/TopStreams/index.tsx
@@ -1,8 +1,10 @@
 import gql from 'graphql-tag';
 import { FC } from 'react';
 import { Query } from 'react-apollo';
+import Link from 'next/link';
 import styled from 'styled-components';
-import { Grid } from '../../ui';
+import { darken, lighten } from 'polished';
+import { Grid, Icon } from '../../ui';
 import { Stream } from './Stream';
 
 const GET_CHANNELS_TOP = gql`
@@ -33,6 +35,31 @@ const StreamColumnBox = styled.div`
   margin-bottom: 14px;
 `;
 
+const StreamAddBox = styled.div`
+  cursor: pointer;
+  height: 100%;
+  min-height: 150px;
+  width: 100%;
+  background: ${({ theme }) => darken(0.04, theme.dark1Color)};
+  border-radius: 4px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  i {
+    font-size: 26px;
+    color: ${({ theme }) => lighten(0.5, theme.dark1Color)};
+  }
+`;
+
+const StreamAddText = styled.div`
+  margin-top: 10px;
+  font-size: 13px;
+  color: ${({ theme }) => lighten(0.5, theme.dark1Color)};
+`;
+
 type StreamsPosition = 'line' | 'column';
 
 interface IProps {
@@ -42,10 +69,26 @@ interface IProps {
   interval?: number;
 }
 
+const StreamAdd = () => (
+  <Link href={`/promoter`} passHref>
+    <StreamAddBox>
+      <Icon type="plus-circle-o" />
+      <StreamAddText>Разместить стрим</StreamAddText>
+    </StreamAddBox>
+  </Link>
+);
+
 export const TopStreams: FC<IProps> = ({ max, live, position, interval }) => (
   <Query query={GET_CHANNELS_TOP} pollInterval={interval}>
     {({ data }) => {
-      const channels = ((data && data.channelsTop) || []).slice(0, max);
+      let channels = ((data && data.channelsTop) || []).slice(0, max);
+      const channelsCount = channels.length;
+
+      console.log(channelsCount);
+
+      if (channelsCount < max) {
+        channels = [...channels, null];
+      }
 
       if (position === 'line') {
         return (
@@ -54,22 +97,42 @@ export const TopStreams: FC<IProps> = ({ max, live, position, interval }) => (
               elementWidth={300}
               maxRows={1}
               items={channels}
-              itemRender={(channel, index) => (
-                <StreamLineBox key={`${channel.id}-${channel.cost}`}>
-                  <Stream {...channel} livePreview={index < live} />
-                </StreamLineBox>
-              )}
+              itemRender={(channel, index) => {
+                if (!channel) {
+                  return (
+                    <StreamLineBox key={`nochannel`}>
+                      <StreamAdd />
+                    </StreamLineBox>
+                  );
+                }
+
+                return (
+                  <StreamLineBox key={`${channel.id}-${channel.cost}`}>
+                    <Stream {...channel} livePreview={index < live} />
+                  </StreamLineBox>
+                );
+              }}
             />
           </StreamsLineBox>
         );
       } else {
         return (
           <StreamsColumnBox>
-            {channels.map((channel, index) => (
-              <StreamColumnBox key={`${channel.id}-${channel.cost}`}>
-                <Stream {...channel} livePreview={index < live} />
-              </StreamColumnBox>
-            ))}
+            {channels.map((channel, index) => {
+              if (!channel) {
+                return (
+                  <StreamColumnBox key={`nochannel`}>
+                    <StreamAdd />
+                  </StreamColumnBox>
+                );
+              }
+
+              return (
+                <StreamColumnBox key={`${channel.id}-${channel.cost}`}>
+                  <Stream {...channel} livePreview={index < live} />
+                </StreamColumnBox>
+              );
+            })}
           </StreamsColumnBox>
         );
       }

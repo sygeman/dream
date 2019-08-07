e1e6a495fb2d96494bb524672eec8a69c043044f
Sygeman <sygeman@gmail.com>
2019-08-07 16:02:21 +0300

    Wallet with apollo hooks

--- providers/Wallet.tsx
+++ providers/Wallet.tsx
@@ -1,8 +1,6 @@
 import gql from 'graphql-tag';
-import { Component, FC } from 'react';
-import { Query } from 'react-apollo';
-
-const PROVIDER_KEY = 'wallet';
+import { FC, useEffect } from 'react';
+import { useQuery } from 'react-apollo';
 
 const GET = gql`
   query getWallet($where: WalletWhereInput!) {
@@ -24,68 +22,43 @@ const UPDATED = gql`
   }
 `;
 
-interface IPropsInner {
-  data: any;
-  updated: () => void;
-  children: any;
-}
-
-class ProviderInner extends Component<IPropsInner> {
-  public componentDidMount() {
-    if (typeof this.props.updated === 'function') {
-      this.props.updated();
-    }
-  }
-
-  public render() {
-    return this.props.children({
-      data: this.props.data
-    });
-  }
-}
-
 interface IProps {
   where?: any;
   children: any;
 }
 
-const Provider: FC<IProps> = ({ children, where }) => (
-  <Query query={GET} variables={{ where }}>
-    {({ loading, error, data, subscribeToMore }) => {
-      if (loading || error || !data || !data.wallet) {
-        return children({ data: null });
-      }
+const Provider: FC<IProps> = ({ children, where }) => {
+  const { loading, error, data, subscribeToMore } = useQuery(GET, {
+    variables: { where }
+  });
 
-      return (
-        <ProviderInner
-          data={data[PROVIDER_KEY]}
-          updated={() =>
-            subscribeToMore({
-              document: UPDATED,
-              variables: { id: data[PROVIDER_KEY].id },
-              updateQuery: (prev, { subscriptionData }) => {
-                if (!subscriptionData.data) {
-                  return prev;
-                }
-
-                const updatedData = subscriptionData.data[PROVIDER_KEY];
-
-                return {
-                  ...prev,
-                  [PROVIDER_KEY]: {
-                    ...prev[PROVIDER_KEY],
-                    ...updatedData
-                  }
-                };
-              }
-            })
+  useEffect(() => {
+    if (data && data.wallet) {
+      subscribeToMore({
+        document: UPDATED,
+        variables: { id: data.wallet.id },
+        updateQuery: (prev, { subscriptionData }) => {
+          if (!subscriptionData.data) {
+            return prev;
           }
-        >
-          {children}
-        </ProviderInner>
-      );
-    }}
-  </Query>
-);
+
+          const updatedData = subscriptionData.data.wallet;
+
+          return {
+            ...prev,
+            wallet: {
+              ...prev.wallet,
+              ...updatedData
+            }
+          };
+        }
+      });
+    }
+  }, [loading]);
+
+  return children({
+    data: loading || error || !data || !data.wallet ? null : data.wallet
+  });
+};
 
 export default Provider;

a3cb0361949223f8904f5997b614d749ee509ddf
Sygeman <sygeman@gmail.com>
2019-08-07 15:46:36 +0300

    delete dead code

--- components/Clip/Clip.tsx
+++ components/Clip/Clip.tsx
@@ -1,12 +1,8 @@
-// import Head from 'next/head';
 import { FC } from 'react';
 import { darken } from 'polished';
 import styled from 'styled-components';
 import { TwitchClipPlayer } from '../../ui';
 import { ClipComments } from './Comments';
-// import PostHelper from '../Post';
-// import { IPost, PostReactionType } from './interfaces/Post';
-// import PostReactionProvider from '../../providers/PostReaction';
 
 const Box = styled.div`
   flex-direction: column;
@@ -17,59 +13,9 @@ const Box = styled.div`
   overflow: hidden;
 `;
 
-// const Top = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   padding: 8px 12px;
-// `;
-
-// const TitleBox = styled.div`
-//   padding: 5px 10px;
-//   font-size: 15px;
-//   display: flex;
-//   width: 100%;
-//   align-items: center;
-// `;
-
-// const Title = styled.div<{ active: boolean }>`
-//   display: flex;
-//   flex: 1;
-//   ${({ active }) => active && `cursor: pointer;`}
-
-//   span {
-//     overflow: hidden;
-//     white-space: nowrap;
-//     text-overflow: ellipsis;
-//     margin-right: 10px;
-//     display: inline;
-//   }
-// `;
-
 const ContentBox = styled.div`
   background: ${({ theme }) => darken(0.1, theme.dark2Color)};
 `;
-
-// const EmptyBottom = styled.div`
-//   height: 100%;
-//   display: flex;
-//   flex: 1;
-// `;
-
-// const ChannelLink = styled.a`
-//   display: flex;
-//   align-items: center;
-//   font-size: 13px;
-//   background: ${({ theme }) => theme.main1Color};
-//   padding: 5px;
-//   border-radius: 3px;
-//   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-
-//   i {
-//     font-size: 15px;
-//     margin-right: 5px;
-//     padding: 0 5px;
-//   }
-// `;
 
 const CommentsBox = styled.div`
   display: flex;
@@ -81,116 +27,21 @@ const CommentsBox = styled.div`
   overflow: hidden;
 `;
 
-// const PostDeleted = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   justify-content: center;
-//   min-height: 200px;
-//   color: ${({ theme }) => theme.accent2Color};
-//   text-transform: uppercase;
-//   font-size: 13px;
-
-//   i {
-//     font-size: 35px;
-//     padding: 20px 0;
-//   }
-// `;
-
 interface IProps {
   clipId?: string;
   autoPlay?: boolean;
 }
 
 export const Clip: FC<IProps> = ({ clipId, autoPlay }) => {
-  // if (deleted) {
-  //   return (
-  //     <Box>
-  //       <PostDeleted>
-  //         <div>
-  //           <Icon type="delete" />
-  //         </div>
-  //         <div>Клип был удален</div>
-  //       </PostDeleted>
-  //     </Box>
-  //   );
-  // }
-
   return (
     <Box>
-      {/* <Head>
-        <title>{title}</title>
-        {meta && (
-          <>
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={title} />
-            <meta property="og:image" content={cover} />
-            <meta
-              property="og:url"
-              content={`https://pepega.com/post?id=${id}`}
-            />
-          </>
-        )}
-      </Head> */}
-      {/* {header && id && (
-        <Top>
-          <TitleBox>
-            <Title active={!meta}>
-              <span>{title}</span>
-            </Title>
-            {channelName && (
-              <ChannelLink
-                href={`https://twitch.tv/${channelName}`}
-                target="_blank"
-              >
-                <Icon type="twitch" />
-                {channelName}
-              </ChannelLink>
-            )}
-          </TitleBox>
-        </Top>
-      )} */}
       <ContentBox>
         <TwitchClipPlayer sourceId={clipId} autoPlay={autoPlay} />
       </ContentBox>
 
-      {/* <PostHelper.Bottom>
-        <PostReactionProvider postId={id}>
-            {({ postReaction }) => {
-              const reaction = postReaction ? postReaction.type : 'none';
-
-              return (
-                <>
-                  <PostHelper.ReactionButton
-                    id={id}
-                    type="like"
-                    state={reaction === PostReactionType.like}
-                    count={likes}
-                    icon="thumb-up"
-                  />
-                  <PostHelper.ReactionButton
-                    id={id}
-                    type="dislike"
-                    state={reaction === PostReactionType.dislike}
-                    count={dislikes}
-                    icon="thumb-down"
-                  />
-                </>
-              );
-            }}
-          </PostReactionProvider>
-
-        <PostHelper.ShareButton id={id} />
-        <PostHelper.Menu id={id} authorId={authorId} />
-        <EmptyBottom />
-        <PostHelper.Author createdAt={createdAt} authorId={authorId} />
-      </PostHelper.Bottom> */}
-
-      {/* {id && ( */}
       <CommentsBox>
         <ClipComments clipId={clipId} />
       </CommentsBox>
-      {/* )} */}
     </Box>
   );
 };

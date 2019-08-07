c6724d2a961071c1abd89f6818afbbb61c524539
Sygeman <sygeman@gmail.com>
2019-08-07 15:32:00 +0300

    Communities with apollo hooks

--- pages/communities.tsx
+++ pages/communities.tsx
@@ -1,13 +1,30 @@
+import gql from 'graphql-tag';
 import Layout from '../layouts/Main';
 import { Communities } from '../components/Community/Communities';
-import CommunitiesProvider from '../providers/Communities';
+import { useQuery } from 'react-apollo';
+
+const GET_COMMUNITIES = gql`
+  query communities {
+    communities {
+      id
+      name
+      description
+      avatar
+    }
+  }
+`;
 
 const CommunitiesPage = () => {
+  let communities = [];
+  const { loading, error, data } = useQuery(GET_COMMUNITIES);
+
+  if (!loading && !error && data && data.communities) {
+    communities = data.communities;
+  }
+
   return (
     <Layout streams>
-      <CommunitiesProvider>
-        {({ communities }) => <Communities communities={communities} />}
-      </CommunitiesProvider>
+      <Communities communities={communities} />
     </Layout>
   );
 };

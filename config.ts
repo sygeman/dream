319aec33cf002614ca8561f0c467e288246b10df
Sygeman <sygeman@gmail.com>
2019-08-16 17:28:40 +0300

    defaultCommunityId

--- config.ts
+++ config.ts
@@ -1,13 +1,14 @@
 const commonConfig = {
   sentryDsn: 'https://a264c8fe670545b88cfbdb41cd4819f0@sentry.io/1525960',
   cdnUrl: 'https://cdn.pepega.com/',
-  discordInvite: 'https://discord.gg/xVprhFC',
+  discordInvite: 'https://discord.gg/xVprhFC'
 };
 
 const devConfig = {
   apiUrl: 'http://localhost:3000/',
   gqlUrl: 'http://localhost:3000/graphql',
   wsgqlUrl: 'ws://localhost:3000/graphql',
+  defaultCommunityId: '4dbda3ee-a71e-4640-b98b-be33b23a2097',
   cookieOptions: {}
 };
 
@@ -15,6 +16,7 @@ const prodConfig = {
   apiUrl: 'https://api.pepega.com/',
   gqlUrl: 'https://api.pepega.com/graphql',
   wsgqlUrl: 'wss://api.pepega.com/graphql',
+  defaultCommunityId: '9b20808c-2e6b-493b-be8a-8133115a0771',
   cookieOptions: {
     domain: '.pepega.com'
   }

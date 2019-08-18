acf0c2437858b6506688929e1529b2b7681760d8
Sygeman <sygeman@gmail.com>
2019-08-18 15:25:43 +0300

    GameChannel without access

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,13 +1,16 @@
 import Game from '../components/Game';
 import Layout from '../layouts/Main';
 import { useRouter } from '../hooks/useRouter';
-import { useAccess } from '../hooks/useAccess';
 
 const GamePage = () => {
   const router = useRouter();
   const gameId = router.query.id;
 
-  return <Layout streams>{useAccess() && <Game gameId={gameId} />}</Layout>;
+  return (
+    <Layout streams>
+      <Game gameId={gameId} />
+    </Layout>
+  );
 };
 
 export default GamePage;

54907a23ec6d49fe4550fe84d20a7b013c005455
Sygeman <sygeman@gmail.com>
2019-08-18 15:25:34 +0300

    PageChannel without access

--- pages/channel.tsx
+++ pages/channel.tsx
@@ -1,14 +1,13 @@
 import Channel from '../components/Channel';
 import Layout from '../layouts/Main';
 import { useRouter } from '../hooks/useRouter';
-import { useAccess } from '../hooks/useAccess';
 
 const ChannelPage = () => {
   const router = useRouter();
 
   return (
     <Layout streams>
-      {useAccess() && <Channel userId={router.query.id} />}
+      <Channel userId={router.query.id} />
     </Layout>
   );
 };

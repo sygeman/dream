0b2d1f2706b67e5072bbe756816c5ba9a063c7c1
Sygeman <sygeman@gmail.com>
2019-09-02 05:04:45 +0300

    LogoBox

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -1,57 +1,22 @@
 import { FC, ReactNode } from 'react';
-import Link from 'next/link';
-import styled from 'styled-components';
-import { lighten, darken, rgba } from 'polished';
-import config from '../config';
 import * as LeftMenu from '../ui/LeftMenu';
 import BaseLayout from './Base';
 import { TopStreams } from '../components/TopStreams';
 import { Categories } from '../components/Nav/Left/Categories';
 import { Follows } from '../components/Nav/Left/Follows';
+import { LogoBox } from '../components/Nav/Left/Logo';
 
 interface IProps {
   fixedTopContent?: ReactNode;
   streams?: boolean;
 }
 
-const LogoLink = styled.a`
-  display: flex;
-  align-items: center;
-  height: 100%;
-  height: 42px;
-  background: ${({ theme }) => rgba(darken(0.06, theme.main1Color), 0.9)};
-  margin-bottom: 10px;
-`;
-
-const LogoImg = styled.img`
-  height: 27px;
-  width: 27px;
-  margin-left: 18px;
-  padding: 5px;
-  cursor: pointer;
-  background: ${({ theme }) => lighten(0.05, theme.main1Color)};
-  border-radius: 5px;
-`;
-
-const LogoTitle = styled.div`
-  font-size: 13px;
-  margin-left: 15px;
-  color: ${({ theme }) => lighten(0.4, theme.main1Color)};
-  letter-spacing: 0.5px;
-  font-weight: 500;
-`;
-
 const MainLayout: FC<IProps> = ({ children, fixedTopContent, streams }) => (
   <BaseLayout
     fixedTopContent={fixedTopContent}
     leftMenu={
       <LeftMenu.Box>
-        <Link href="/" passHref>
-          <LogoLink>
-            <LogoImg src={`${config.cdnUrl}logo.svg`} />
-            <LogoTitle>PepegaCom</LogoTitle>
-          </LogoLink>
-        </Link>
+        <LogoBox />
         <LeftMenu.Item route="/" equal icon="home" title="Главная" />
         <LeftMenu.Item equal route="/new" icon="flare" title="Новое" />
         <LeftMenu.Item route="/top" icon="trending-up" title="Топ" />
--- layouts/Manage.tsx
+++ layouts/Manage.tsx
@@ -2,6 +2,7 @@ import { FC, ReactNode } from 'react';
 import { useAccess } from '../hooks/useAccess';
 import * as LeftMenu from '../ui/LeftMenu';
 import BaseLayout from './Base';
+import { LogoBox } from '../components/Nav/Left/Logo';
 
 interface IProps {
   fixedTopContent?: ReactNode;
@@ -19,6 +20,7 @@ const ManageLayout: FC<IProps> = ({ children, fixedTopContent }) => {
       fixedTopContent={fixedTopContent}
       leftMenu={
         <LeftMenu.Box>
+          <LogoBox />
           <LeftMenu.Item route="/manage" equal icon="chart" title="Dashboard" />
         </LeftMenu.Box>
       }

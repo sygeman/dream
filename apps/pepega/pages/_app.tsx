a6a47cbc69448131aa563b9dd88a91990f9e6fb2
Sygeman <sygeman@gmail.com>
2021-02-11 18:51:32 +0000

    Removed nprogress

--- apps/pepega/pages/_app.tsx
+++ apps/pepega/pages/_app.tsx
@@ -2,15 +2,10 @@ import React from 'react';
 import App from 'next/app';
 import Head from 'next/head';
 import Router from 'next/router';
-import NProgress from 'nprogress';
 import 'resize-observer-polyfill';
 import { withApollo } from 'src/lib/apollo';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle, themes } from 'src/themes';
-
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
 
 interface IProps {
   isServer: boolean;

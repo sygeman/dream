2f22b6703cae0046ecb24c3dccb1d1894fcfeddb
Sygeman <sygeman@gmail.com>
2021-02-11 19:30:42 +0000

    Removed themes

--- apps/pepega/pages/_app.tsx
+++ apps/pepega/pages/_app.tsx
@@ -5,7 +5,60 @@ import Router from 'next/router';
 import 'resize-observer-polyfill';
 import { withApollo } from 'src/lib/apollo';
 import { ThemeProvider } from 'styled-components';
-import { GlobalStyle, themes } from 'src/themes';
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Orbitron:500|Roboto:300,400,500&subset=cyrillic-ext');
+
+  @keyframes fadeIn{
+    0%{
+      opacity: 0;
+    }
+    100%{
+      opacity: 1;
+    }
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    min-height: 0;
+    min-width: 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  html, body {
+    height: 100%;
+    font-family: 'Roboto', sans-serif;
+    overflow-y: hidden;
+    background: #1E1D22;
+    color: #eee;
+  }
+
+  #__next,
+  #__layout {
+    height: 100%;
+    width: 100%;
+    overflow-y: hidden;
+    outline: none;
+  }
+
+  .rc-dropdown {
+    position: absolute;
+    z-index: 1070;
+  }
+
+  .rc-dropdown-hidden {
+    display: none;
+  }
+`;
 
 interface IProps {
   isServer: boolean;
@@ -17,7 +70,17 @@ class MyApp extends App<IProps> {
     const { Component, pageProps } = this.props;
 
     return (
-      <ThemeProvider theme={themes.dark}>
+      <ThemeProvider
+        theme={{
+          colors: {
+            primary: '#6441A4',
+            surface: '#262841',
+            background: '#1D1E31',
+            accent: '#968A9D',
+            text: '#EEEEEE',
+          },
+        }}
+      >
         <>
           <Head>
             <title>PepegaCom</title>

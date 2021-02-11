194a893e2c7bd59a8432be134648ac955828dce3
Sygeman <sygeman@gmail.com>
2021-02-11 19:26:26 +0000

    Removed nprogress styles

--- apps/pepega/themes/global.ts
+++ apps/pepega/themes/global.ts
@@ -51,29 +51,4 @@ export const GlobalStyle = createGlobalStyle`
   .rc-dropdown-hidden {
     display: none;
   }
-
-  #nprogress {
-    pointer-events: none;
-  }
-
-  #nprogress .bar {
-    background: #6441A4;
-    position: fixed;
-    z-index: 1031;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 2px;
-  }
-
-  #nprogress .peg {
-    display: block;
-    position: absolute;
-    right: 0px;
-    width: 100px;
-    height: 100%;
-    box-shadow: 0 0 10px #fff, 0 0 5px #fff;
-    opacity: 1.0;
-    transform: rotate(3deg) translate(0px, -4px);
-  }
 `;

d3f0be3ada4723ad4d1a9f22474e028a36af8561
Sygeman <sygeman@gmail.com>
2021-02-16 20:39:51 +0000

    Fixed main scroll

--- libs/containers/src/main-communities/index.tsx
+++ libs/containers/src/main-communities/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import SimpleBar from 'simplebar-react';
 
 const CommunityCard: React.FC<{ title: string; name: string }> = ({
   title,
@@ -9,9 +10,6 @@ const CommunityCard: React.FC<{ title: string; name: string }> = ({
   return (
     <Link href={`/${name}`}>
       <div className="flex flex-col flex-shrink-0 overflow-hidden items-center justify-center  cursor-pointer hover:opacity-90 bg-surface m-4 rounded">
-        {/* <div className="rounded-full bg-background h-32px w-32px flex items-center justify-center justify-items-start">
-          <span className="text-gray-400 text-sm">{name}</span>
-        </div> */}
         <div className="w-full bg-surface-light py-12"></div>
         <div className="w-full px-4 py-2">
           <span className="text-sm text-text">{title}</span>
@@ -44,15 +42,17 @@ export const MainCommunities = () => {
       </div>
 
       <div className="flex flex-1 w-full overflow-hidden my-4">
-        <div className="w-full grid grid-cols-fill-240px auto-rows-max gap-2 justify-center overflow-y-auto">
-          {[...Array(100).keys()].map((k) => (
-            <CommunityCard
-              key={k}
-              name={`community-${k}`}
-              title={`Community ${k}`}
-            />
-          ))}
-        </div>
+        <SimpleBar className="h-full w-full">
+          <div className="w-full grid grid-cols-fill-240px auto-rows-max gap-2 justify-center overflow-y-auto">
+            {[...Array(100).keys()].map((k) => (
+              <CommunityCard
+                key={k}
+                name={`community-${k}`}
+                title={`Community ${k}`}
+              />
+            ))}
+          </div>
+        </SimpleBar>
       </div>
     </div>
   );

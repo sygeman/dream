953f9e04bd20f748ed5fb87194e0b4dd594e1c9a
Sygeman <sygeman@gmail.com>
2021-03-16 19:15:50 +0000

    Added purge in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,37 @@ const twitch = '#6542a6';
 const spotify = '#1a9c48';
 const youtube = '#dc1b1b';
 
+const colors = {
+  primary: {
+    DEFAULT: primary,
+    light: lighen(primary, 0.2),
+  },
+  backgorud: {
+    DEFAULT: backgorud,
+    light: lighen(backgorud, 0.2),
+  },
+  accent: {
+    DEFAULT: accent,
+    light: lighen(accent, 0.4),
+  },
+  surface: {
+    DEFAULT: surface,
+    light: lighen(surface, 0.4),
+  },
+  twitch: {
+    light: lighen(twitch, 0.2),
+    DEFAULT: twitch,
+  },
+  spotify: {
+    light: lighen(spotify, 0.2),
+    DEFAULT: spotify,
+  },
+  youtube: {
+    light: lighen(youtube, 0.2),
+    DEFAULT: youtube,
+  },
+};
+
 module.exports = {
   darkMode: 'media',
   purge: {
@@ -17,36 +48,13 @@ module.exports = {
       'apps/dream/pages/**/*.{js,ts,jsx,tsx}',
       'libs/**/*.{js,ts,jsx,tsx}',
     ],
+    options: {
+      safelist: Object.keys(colors).map((color) => new RegExp(`\-${color}`)),
+    },
   },
   theme: {
     extend: {
-      colors: {
-        primary: {
-          DEFAULT: primary,
-          light: lighen(primary, 0.2),
-        },
-        backgorud,
-        accent: {
-          DEFAULT: accent,
-          light: lighen(accent, 0.4),
-        },
-        surface: {
-          DEFAULT: surface,
-          light: lighen(surface, 0.4),
-        },
-        twitch: {
-          light: lighen(twitch, 0.2),
-          DEFAULT: twitch,
-        },
-        spotify: {
-          light: lighen(spotify, 0.2),
-          DEFAULT: spotify,
-        },
-        youtube: {
-          light: lighen(youtube, 0.2),
-          DEFAULT: youtube,
-        },
-      },
+      colors,
       fontFamily: {
         sans: 'Roboto',
       },

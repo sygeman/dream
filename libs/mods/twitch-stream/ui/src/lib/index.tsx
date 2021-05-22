8420301eb72aa45a1b0ec5987a55d970a4350bb3
Sygeman <sygeman@gmail.com>
2021-05-22 19:44:45 +0000

    Player with css aspect ratio instead js method

--- libs/mods/twitch-stream/ui/src/lib/index.tsx
+++ libs/mods/twitch-stream/ui/src/lib/index.tsx
@@ -1,28 +1,9 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
 import { useRouter } from 'next/router';
-import { useResizeDetector } from 'react-resize-detector';
 import { useChannelQuery, useTwitchStreamQuery } from '@dream/types';
 
 export const ChannelModeTwitchStream = () => {
-  const { width, height, ref } = useResizeDetector();
-  const heightWithMargin = height - 40;
-
-  // By width
-  let w = width;
-  let h = (width * 9) / 16;
-
-  // By height
-  if (h >= heightWithMargin) {
-    h = heightWithMargin;
-    w = (heightWithMargin * 16) / 9;
-  }
-
-  if (isNaN(w) || isNaN(h)) {
-    h = 0;
-    w = 0;
-  }
-
   const { query } = useRouter();
   const channelName = typeof query?.channel === 'string' && query?.channel;
 
@@ -39,14 +20,10 @@ export const ChannelModeTwitchStream = () => {
 
   const channelKey = twitchStreamQuery?.data?.twitchStream?.channelKey;
 
-  if (!channelKey) {
-    return null;
-  }
-
   return (
-    <div ref={ref} className="h-full w-full bg-background-light">
-      <div className="bg-black flex justify-center">
-        <div style={{ width: w, height: h }}>
+    <div className="h-full w-full bg-background-light">
+      <div className="bg-black flex justify-center aspect-w-16 aspect-h-9">
+        {channelKey && (
           <ReactPlayer
             url={`https://www.twitch.tv/${channelKey}`}
             height="100%"
@@ -54,7 +31,7 @@ export const ChannelModeTwitchStream = () => {
             // playing
             muted
           />
-        </div>
+        )}
       </div>
     </div>
   );

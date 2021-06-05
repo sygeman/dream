019d096936afdb0e95a39d916143f775604c4f04
Sygeman <sygeman@gmail.com>
2021-06-05 16:39:39 +0000

    Added waitlist spotify auth lock screen

--- libs/mods/waitlist-spotify/ui/src/lib/index.tsx
+++ libs/mods/waitlist-spotify/ui/src/lib/index.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import {
+  useMeQuery,
   useWaitlistSpotifyCurrentQuery,
   useWaitlistSpotifyCurrentUpdatedSubscription,
   useWaitlistSpotifyUserSyncMutation,
 } from '@dream/types';
+import { AuthButtonSpotify } from '@dream/auth';
 import { Backgroud } from './components/background';
 import { ChannelModeWaitlistSpotifyHistory } from './history';
 import { ChannelModeWaitlistSpotifyQueue } from './queue';
@@ -11,6 +13,10 @@ import { ChannelModeWaitlistSpotifyCurrent } from './current';
 import { useChannelId } from './use-channel-id';
 
 export const ChannelModeWaitlistSpotify = () => {
+  const meQuery = useMeQuery();
+  const profiles = meQuery?.data?.me?.profiles || [];
+  const hasSpotifyAccount = !!profiles.find((p) => p.provider === 'spotify');
+
   const channelId = useChannelId();
   const [isConnected, setIsConnected] = useState(false);
 
@@ -45,7 +51,17 @@ export const ChannelModeWaitlistSpotify = () => {
   return (
     <div className="h-screen w-full flex flex-1 flex-col relative overflow-hidden">
       <Backgroud imageUrl={current?.cover} />
-      <div className="absolute left-0 top-0 w-full h-full flex flex-col">
+      <div
+        className="absolute left-0 top-0 w-full h-full flex flex-col"
+        style={
+          hasSpotifyAccount
+            ? undefined
+            : {
+                filter: 'blur(2px)',
+                opacity: 0.5,
+              }
+        }
+      >
         <ChannelModeWaitlistSpotifyHistory />
         <ChannelModeWaitlistSpotifyCurrent
           current={current}
@@ -54,6 +70,25 @@ export const ChannelModeWaitlistSpotify = () => {
         />
         <ChannelModeWaitlistSpotifyQueue isConnected={isConnected} />
       </div>
+      {!hasSpotifyAccount && (
+        <div className="absolute left-0 top-0 w-full h-full flex items-center justify-center">
+          <div className="flex flex-col">
+            <div className="text-sm text-accent mb-2">
+              For this mode you need a{' '}
+              <a
+                href="https://www.spotify.com/premium/"
+                target="_blank"
+                rel="noreferrer"
+                className="text-white underline"
+              >
+                Spotify Premium
+              </a>{' '}
+              account
+            </div>
+            <AuthButtonSpotify />
+          </div>
+        </div>
+      )}
     </div>
   );
 };

f20361cb483b351168b08b3cda756e5f87ac0bbc
Sygeman <sygeman@gmail.com>
2021-11-05 14:34:44 +0000

    Updated MainCommunities

--- libs/community/src/lib/main-communities/index.tsx
+++ libs/community/src/lib/main-communities/index.tsx
@@ -13,7 +13,11 @@ const CommunityCard: React.FC<{
   return (
     <Link href={`/${name}`}>
       <div className="flex flex-col flex-shrink-0 overflow-hidden items-center justify-center  cursor-pointer hover:opacity-90 bg-surface-light m-4 rounded">
-        <div className="w-full bg-background aspect-w-16 aspect-h-9"></div>
+        <div className="w-full bg-background aspect-w-16 aspect-h-9 relative">
+          <div className="absolute h-full w-full flex justify-center items-center overflow-hidden text-accent opacity-25 text-3xl">
+            {title}
+          </div>
+        </div>
         <div className="flex items-center justify-between w-full px-4 py-2 border-t border-surface">
           <span className="text-sm text-white">{title}</span>
           <span className="text-accent text-xs rounded bg-surface px-2 py-1">
@@ -37,41 +41,31 @@ export const MainCommunities = () => {
     <div className="flex flex-col w-full bg-surface">
       <div className="flex flex-1 w-full overflow-hidden">
         <SimpleBar className="h-full w-full">
-          <div className="flex items-center bg-gradient-to-r from-primary to-primary-dark w-full py-2 px-4">
-            <div className="text-sm flex-1">
-              <p className="text-white font-medium">
-                Build your community based on media modules
-              </p>
-              <p className="text-white opacity-50 font-medium">
-                The project is under development, communities created now can be
-                deleted, this is an open source project and is available on{' '}
-                <a
-                  className="underline"
-                  target="_blank"
-                  href="https://github.com/sygeman/dream"
-                >
-                  GitHub
-                </a>
-              </p>
+          <div className="flex items-center mt-6 px-6">
+            <div className="flex flex-col">
+              <span className="text-lg">Communities</span>
+              <span className="text-sm text-accent">
+                List of all communities
+              </span>
             </div>
-            <div>
-              <Link
-                href={{
-                  pathname: router.route,
-                  query: {
-                    ...router.query,
-                    [isUser ? 'newCommunity' : 'authModal']: 1,
-                  },
-                }}
-                passHref
-              >
-                <a href="repalce">
-                  <button className="btn btn-primary">Create Community</button>
-                </a>
-              </Link>
-            </div>
+
+            <Link
+              href={{
+                pathname: router.route,
+                query: {
+                  ...router.query,
+                  [isUser ? 'newCommunity' : 'authModal']: 1,
+                },
+              }}
+              passHref
+            >
+              <a href="repalce">
+                <button className="btn btn-primary ml-4 h-8">
+                  Create Community
+                </button>
+              </a>
+            </Link>
           </div>
-          <div className="mt-6 px-4 text-lg text-accent">Communities</div>
           <div
             className={clsx(
               'w-full grid auto-rows-max gap-2 justify-center overflow-y-auto',

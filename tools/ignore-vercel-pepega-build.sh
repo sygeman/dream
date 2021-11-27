37733958ec57ebe4adf34e2960f248cd94c112c5
Sygeman <sygeman@gmail.com>
2021-11-27 12:58:00 +0000

    Updated ignore build

--- tools/ignore-vercel-pepega-build.sh
+++ tools/ignore-vercel-pepega-build.sh
@@ -3,12 +3,12 @@
 APP=pepega
 
 # Determine version of Nx installed
-NX_VERSION=$(node -e "console.log(require('./package.json').devDependencies['@nrwl/workspace'])")
-TS_VERSION=$(node -e "console.log(require('./package.json').devDependencies['typescript'])")
+# NX_VERSION=$(node -e "console.log(require('./package.json').devDependencies['@nrwl/workspace'])")
+# TS_VERSION=$(node -e "console.log(require('./package.json').devDependencies['typescript'])")
 
 # Install @nrwl/workspace in order to run the affected command
-yarn install --dev @nrwl/workspace@$NX_VERSION
-yarn install --dev typescript@$TS_VERSION
+# yarn install --dev @nrwl/workspace@$NX_VERSION
+# yarn install --dev typescript@$TS_VERSION
 
 # Run the affected command, comparing latest commit to the one before that
 yarn nx affected:apps --plain --base HEAD~1 --head HEAD | grep $APP -q

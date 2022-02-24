2681c8a97aa5fd068507f675f2dcef53b7999fd6
Sygeman <sygeman@gmail.com>
2022-02-24 19:34:45 +0000

    Removed _document

--- apps/pepega/pages/_app.tsx
+++ apps/pepega/pages/_app.tsx
@@ -34,7 +34,12 @@ const Inner = ({ Component, pageProps, locale }) => {
   return (
     <IntlProvider locale={locale} messages={lang[locale]}>
       <Head>
-        <title>Pepega</title>
+        <title>PepegaCom</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Orbitron:500|Roboto:300,400,500&subset=cyrillic-ext"
+          rel="stylesheet"
+        />
         <link
           rel="icon"
           type="image/png"

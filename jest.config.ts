840a7730dc6d825fbb49cc49bb01fe19df752626
Sygeman <sygeman@gmail.com>
2022-05-30 19:25:56 +0000

    Added server-state api

--- jest.config.ts
+++ jest.config.ts
@@ -71,5 +71,6 @@ module.exports = {
     '<rootDir>/libs/pepega/components/video-preview',
     '<rootDir>/libs/pepega/utils/count',
     '<rootDir>/apps/server-state',
+    '<rootDir>/apps/server-state-api',
   ],
 };

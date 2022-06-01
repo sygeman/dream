323fb6d2362caa46b1a359131d4d6844b96463ae
Sygeman <sygeman@gmail.com>
2022-06-01 22:17:08 +0000

    Fixed pepega graphql codegen

--- libs/pepega/types/src/index.tsx
+++ libs/pepega/types/src/index.tsx
@@ -14,6 +14,47 @@ export type Scalars = {
   DateTime: any;
 };
 
+export type Clip = {
+  __typename?: 'Clip';
+  broadcaster_id: Scalars['String'];
+  createdAt: Scalars['String'];
+  created_at: Scalars['String'];
+  creator_id: Scalars['String'];
+  game_id: Scalars['String'];
+  id: Scalars['String'];
+  language: Scalars['String'];
+  score: Scalars['Float'];
+  sourceUrl: Scalars['String'];
+  thumbnail_url: Scalars['String'];
+  title: Scalars['String'];
+  updatedAt: Scalars['String'];
+  video_id: Scalars['String'];
+};
+
+export type ClipComment = {
+  __typename?: 'ClipComment';
+  clipId: Scalars['String'];
+  content: Scalars['String'];
+  createdAt: Scalars['String'];
+  id: Scalars['String'];
+  updatedAt: Scalars['String'];
+  user: ClipCommentUser;
+  userId: Scalars['String'];
+};
+
+export type ClipCommentCreateInput = {
+  clipId: Scalars['String'];
+  content: Scalars['String'];
+};
+
+export type ClipCommentUser = {
+  __typename?: 'ClipCommentUser';
+  avatar?: Maybe<Scalars['String']>;
+  id: Scalars['String'];
+  name?: Maybe<Scalars['String']>;
+  role?: Maybe<UserRole>;
+};
+
 export enum Locale {
   EnUs = 'en_US',
   RuRu = 'ru_RU'
@@ -21,9 +62,39 @@ export enum Locale {
 
 export type Mutation = {
   __typename?: 'Mutation';
+  createClipComment: Scalars['Boolean'];
+  decreaseClipScore: Scalars['Boolean'];
+  increaseClipScore: Scalars['Boolean'];
   logout: Scalars['Boolean'];
+  removeClipComment: Scalars['Boolean'];
+  setClipHistory: Scalars['Boolean'];
   setUserLocale: Locale;
   updateConnectionStatus: Scalars['Boolean'];
+};
+
+
+export type MutationCreateClipCommentArgs = {
+  input: ClipCommentCreateInput;
+};
+
+
+export type MutationDecreaseClipScoreArgs = {
+  clipId: Scalars['String'];
+};
+
+
+export type MutationIncreaseClipScoreArgs = {
+  clipId: Scalars['String'];
+};
+
+
+export type MutationRemoveClipCommentArgs = {
+  id: Scalars['ID'];
+};
+
+
+export type MutationSetClipHistoryArgs = {
+  clipId: Scalars['String'];
 };
 
 
@@ -41,13 +112,56 @@ export type Profile = {
 
 export type Query = {
   __typename?: 'Query';
+  clip?: Maybe<Clip>;
+  clipComments: Array<ClipComment>;
+  clipScore: Scalars['Int'];
+  clips?: Maybe<Array<Clip>>;
   me: User;
   user?: Maybe<User>;
+  userCoins: Scalars['Int'];
+};
+
+
+export type QueryClipArgs = {
+  id: Scalars['String'];
+};
+
+
+export type QueryClipCommentsArgs = {
+  clipId: Scalars['ID'];
+};
+
+
+export type QueryClipScoreArgs = {
+  clipId: Scalars['String'];
 };
 
 
 export type QueryUserArgs = {
   id?: InputMaybe<Scalars['ID']>;
+};
+
+export type Subscription = {
+  __typename?: 'Subscription';
+  clipCommentCreated: ClipComment;
+  clipCommentRemoved: Scalars['ID'];
+  clipScoreUpdated: Scalars['Int'];
+  userCoinsUpdated: Scalars['Int'];
+};
+
+
+export type SubscriptionClipCommentCreatedArgs = {
+  clipId: Scalars['ID'];
+};
+
+
+export type SubscriptionClipCommentRemovedArgs = {
+  clipId: Scalars['ID'];
+};
+
+
+export type SubscriptionClipScoreUpdatedArgs = {
+  clipId: Scalars['String'];
 };
 
 export type User = {

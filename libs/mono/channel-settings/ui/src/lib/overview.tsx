c8d0684c636b5aca6eb17608f15b4c6f7e864836
Sygeman <sygeman@gmail.com>
2022-06-04 20:45:45 +0000

    ChannelSettingsOverview with react-hook-form

--- libs/mono/channel-settings/ui/src/lib/overview.tsx
+++ libs/mono/channel-settings/ui/src/lib/overview.tsx
@@ -1,69 +1,56 @@
-import React from 'react';
-import * as Yup from 'yup';
-import { useFormik } from 'formik';
+import React, { useEffect } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { useUpdateChannelSettingsMutation } from './channel-settings.api';
 import { useCommunityChannel } from '@dream/mono-use-community-channel';
 import { urlNameRegExp } from '@dream/mono-utils-regexp-url-name';
 import { SaveFormPanel } from '@dream/mono-components-save-form';
 
-const ValidationSchema = Yup.object().shape({
-  title: Yup.string()
-    .min(1, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  name: Yup.string()
-    .min(1, 'Too Short!')
-    .max(50, 'Too Long!')
-    .matches(urlNameRegExp)
-    .lowercase()
-    .required('Required'),
-});
-
+interface IFormInput {
+  name: string;
+  title: string;
+}
 export const ChannelSettingsOverview: React.FC = () => {
   const origin = typeof window !== 'undefined' ? window?.location?.origin : '';
   const { community, channel, channelId, communityId } = useCommunityChannel();
+  const defaultValues = {
+    name: channel?.name,
+    title: channel?.title,
+  };
 
-  const formik = useFormik({
-    initialValues: {
-      name: channel?.name,
-      title: channel?.title,
-    },
-    validationSchema: ValidationSchema,
-    onSubmit: (values) => {
-      updateChannelSettings({
-        variables: { input: { ...values, communityId, channelId } },
-      });
-    },
-  });
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { isDirty },
+  } = useForm<IFormInput>();
 
   const [updateChannelSettings] = useUpdateChannelSettingsMutation({
     onCompleted: (data) => {
-      formik.resetForm({
-        values: {
-          name: data?.updateChannelSettings?.name,
-          title: data?.updateChannelSettings?.title,
-        },
+      reset({
+        name: data?.updateChannelSettings?.name,
+        title: data?.updateChannelSettings?.title,
       });
     },
   });
 
-  const isError = Object.keys(formik.errors).length > 0;
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    updateChannelSettings({
+      variables: { input: { ...data, communityId, channelId } },
+    });
+  };
+
+  useEffect(() => {
+    reset(defaultValues);
+  }, [channel]);
 
   return (
-    <form onSubmit={formik.handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor="title" className="text-accent text-xs">
         Title
       </label>
       <input
-        id="title"
-        name="title"
-        type="text"
         autoFocus
-        minLength={1}
-        maxLength={50}
-        placeholder="Awesome Channel"
-        onChange={formik.handleChange}
-        value={formik.values.title}
+        {...register('title', { required: true, minLength: 1, maxLength: 50 })}
         className="bg-background text-white text-xs p-2 rounded w-full focus:outline-none focus:ring-1 mb-2"
       />
 
@@ -72,19 +59,18 @@ export const ChannelSettingsOverview: React.FC = () => {
           {origin}/{community?.name}/
         </label>
         <input
-          id="name"
-          name="name"
-          type="text"
-          minLength={1}
-          maxLength={50}
+          {...register('name', {
+            required: true,
+            minLength: 1,
+            maxLength: 50,
+            pattern: urlNameRegExp,
+          })}
           placeholder="awesome"
-          onChange={formik.handleChange}
-          value={formik.values.name}
           className="bg-background text-white text-xs p-2 rounded w-full focus:outline-none focus:ring-1"
         />
       </div>
 
-      <SaveFormPanel show={formik.dirty} reset={() => formik.resetForm()} />
+      <SaveFormPanel show={isDirty} reset={() => reset(defaultValues)} />
     </form>
   );
 };

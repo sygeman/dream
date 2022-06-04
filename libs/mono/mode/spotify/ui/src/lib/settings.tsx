c90982abeba6c0a92166b6a7a86a8ccc8f1da8e5
Sygeman <sygeman@gmail.com>
2022-06-04 21:13:21 +0000

    Mode Settings with react-hook-form

--- libs/mono/mode/spotify/ui/src/lib/settings.tsx
+++ libs/mono/mode/spotify/ui/src/lib/settings.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { RadioGroup } from '@headlessui/react';
-import * as Yup from 'yup';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle, faDotCircle } from '@fortawesome/free-regular-svg-icons';
-import { useFormik } from 'formik';
 import {
   useSpotifyModeQuery,
   useUpdateSpotifyModeMutation,
@@ -12,10 +11,11 @@ import { SaveFormPanel } from '@dream/mono-components-save-form';
 import clsx from 'clsx';
 import { strategies } from './strategies';
 import { useCommunityChannel } from '@dream/mono-use-community-channel';
+import { SpotifyModeStrategy } from '@dream/mono-types';
 
-const ValidationSchema = Yup.object().shape({
-  strategy: Yup.string().required('Required'),
-});
+interface IFormInput {
+  strategy: SpotifyModeStrategy;
+}
 
 export const ChannelSpotifyModeSettings = () => {
   const { channel } = useCommunityChannel();
@@ -24,77 +24,89 @@ export const ChannelSpotifyModeSettings = () => {
     variables: { channelId: channel?.id },
     skip: !channel?.id,
   });
-  const [updateSpotifyModeMutation] = useUpdateSpotifyModeMutation({
-    onCompleted: (data) => {
-      formik.resetForm({
-        values: {
-          strategy: data?.updateSpotifyMode?.strategy,
-        },
-      });
-    },
-  });
 
   const spotifyMode = spotifyModeQuery?.data?.spotifyMode;
   const strategy = spotifyMode?.strategy;
 
-  const formik = useFormik({
-    initialValues: {
-      strategy,
-    },
-    enableReinitialize: true,
-    validationSchema: ValidationSchema,
-    onSubmit: (values) => {
-      updateSpotifyModeMutation({
-        variables: { input: { ...values, channelId: channel?.id } },
+  const defaultValues = { strategy };
+
+  const {
+    control,
+    reset,
+    handleSubmit,
+    formState: { isDirty },
+  } = useForm<IFormInput>();
+
+  const [updateSpotifyModeMutation] = useUpdateSpotifyModeMutation({
+    onCompleted: (data) => {
+      reset({
+        strategy: data?.updateSpotifyMode?.strategy,
       });
     },
   });
 
+  useEffect(() => {
+    reset(defaultValues);
+  }, [strategy]);
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    updateSpotifyModeMutation({
+      variables: { input: { ...data, channelId: channel?.id } },
+    });
+  };
+
   return (
-    <form onSubmit={formik.handleSubmit} className="w-full">
+    <form onSubmit={handleSubmit(onSubmit)} className="w-full">
       <div className="flex w-full mb-2">
-        <RadioGroup
-          className="w-full"
-          value={formik.values.strategy}
-          onChange={(v) => formik.setFieldValue('strategy', v)}
-        >
-          <RadioGroup.Label className="text-accent text-xs">
-            Strategy
-          </RadioGroup.Label>
-          <div className="flex flex-col w-full">
-            {strategies.map((strategy) => (
-              <RadioGroup.Option key={strategy.id} value={strategy.value}>
-                {({ checked }) => (
-                  <div className="flex w-full rounded my-1 bg-background hover:bg-background-light cursor-pointer">
-                    <div className="px-4 flex items-center">
-                      <FontAwesomeIcon
-                        icon={checked ? faDotCircle : faCircle}
-                        className={clsx(
-                          'h-5 w-5',
-                          checked ? 'text-white' : 'text-accent opacity-80'
-                        )}
-                      />
-                    </div>
-                    <div className="py-2">
-                      <RadioGroup.Label as="div" className="text-sm">
-                        {strategy.name}
-                      </RadioGroup.Label>
-                      <RadioGroup.Description
-                        as="div"
-                        className="text-xs text-accent opacity-80"
-                      >
-                        {strategy.description}
-                      </RadioGroup.Description>
-                    </div>
-                  </div>
-                )}
-              </RadioGroup.Option>
-            ))}
-          </div>
-        </RadioGroup>
+        <Controller
+          render={({ field }) => (
+            <RadioGroup
+              className="w-full"
+              value={field.value}
+              onChange={field.onChange}
+            >
+              <RadioGroup.Label className="text-accent text-xs">
+                Strategy
+              </RadioGroup.Label>
+              <div className="flex flex-col w-full">
+                {strategies.map((strategy) => (
+                  <RadioGroup.Option key={strategy.id} value={strategy.value}>
+                    {({ checked }) => (
+                      <div className="flex w-full rounded my-1 bg-background hover:bg-background-light cursor-pointer">
+                        <div className="px-4 flex items-center">
+                          <FontAwesomeIcon
+                            icon={checked ? faDotCircle : faCircle}
+                            className={clsx(
+                              'h-5 w-5',
+                              checked ? 'text-white' : 'text-accent opacity-80'
+                            )}
+                          />
+                        </div>
+                        <div className="py-2">
+                          <RadioGroup.Label as="div" className="text-sm">
+                            {strategy.name}
+                          </RadioGroup.Label>
+                          <RadioGroup.Description
+                            as="div"
+                            className="text-xs text-accent opacity-80"
+                          >
+                            {strategy.description}
+                          </RadioGroup.Description>
+                        </div>
+                      </div>
+                    )}
+                  </RadioGroup.Option>
+                ))}
+              </div>
+            </RadioGroup>
+          )}
+          name="strategy"
+          control={control}
+          defaultValue={strategy}
+        />
       </div>
 
-      <SaveFormPanel show={formik.dirty} reset={() => formik.resetForm()} />
+      <SaveFormPanel show={isDirty} reset={() => reset(defaultValues)} />
     </form>
   );
 };
--- libs/mono/mode/twitch/ui/src/lib/settings.tsx
+++ libs/mono/mode/twitch/ui/src/lib/settings.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import * as Yup from 'yup';
-import { useFormik } from 'formik';
+import React, { useEffect } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import {
   useTwitchStreamQuery,
   useUpdateTwitchStreamMutation,
@@ -8,14 +7,9 @@ import {
 import { SaveFormPanel } from '@dream/mono-components-save-form';
 import { useCommunityChannel } from '@dream/mono-use-community-channel';
 
-const ValidationSchema = Yup.object().shape({
-  channelKey: Yup.string()
-    .min(1, 'Too Short!')
-    .max(50, 'Too Long!')
-    .matches(/[0-9a-zA-Z_]+/)
-    .lowercase()
-    .required('Required'),
-});
+interface IFormInput {
+  channelKey: string;
+}
 
 export const ChannelTwitchModeSettings = () => {
   const { channelId } = useCommunityChannel();
@@ -24,54 +18,56 @@ export const ChannelTwitchModeSettings = () => {
     variables: { channelId },
     skip: !channelId,
   });
-  const [updateTwitchStream] = useUpdateTwitchStreamMutation({
-    onCompleted: (data) => {
-      formik.resetForm({
-        values: {
-          channelKey: data?.updateTwitchStream?.channelKey,
-        },
-      });
-    },
-  });
 
   const twitchStream = twitchStreamQuery?.data?.twitchStream;
   const channelKey = twitchStream?.channelKey;
 
-  const formik = useFormik({
-    initialValues: {
-      channelKey,
-    },
-    validationSchema: ValidationSchema,
-    onSubmit: (values) => {
-      updateTwitchStream({
-        variables: { input: { ...values, channelId } },
+  const defaultValues = { channelKey };
+
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { isDirty },
+  } = useForm<IFormInput>();
+
+  const [updateTwitchStream] = useUpdateTwitchStreamMutation({
+    onCompleted: (data) => {
+      reset({
+        channelKey: data?.updateTwitchStream?.channelKey,
       });
     },
   });
 
-  const isError = Object.keys(formik.errors).length > 0;
+  useEffect(() => {
+    reset(defaultValues);
+  }, [channelKey]);
+
+  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+    updateTwitchStream({
+      variables: { input: { ...data, channelId } },
+    });
+  };
 
   return (
-    <form onSubmit={formik.handleSubmit} className="w-full">
+    <form onSubmit={handleSubmit(onSubmit)} className="w-full">
       <div className="flex items-center mb-2">
         <label htmlFor="channelKey" className="text-accent text-xs mr-2">
           https://www.twitch.tv/
         </label>
         <input
           autoFocus
-          id="channelKey"
-          name="channelKey"
-          type="text"
-          minLength={1}
-          maxLength={50}
+          {...register('channelKey', {
+            required: true,
+            minLength: 1,
+            maxLength: 50,
+            pattern: /[0-9a-zA-Z_]+/,
+          })}
           placeholder="sygeman"
-          onChange={formik.handleChange}
-          value={formik.values.channelKey}
           className="bg-background text-white text-xs p-2 rounded w-full focus:outline-none focus:ring-1"
         />
       </div>
-
-      <SaveFormPanel show={formik.dirty} reset={() => formik.resetForm()} />
+      <SaveFormPanel show={isDirty} reset={() => reset(defaultValues)} />
     </form>
   );
 };

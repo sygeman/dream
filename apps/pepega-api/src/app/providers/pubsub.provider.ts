d3db12f0bc9d5fe9356d9c98ac4790d80895ee5c
Anton Savitskiy <sygeman@gmail.com>
2022-08-24 00:38:35 +0200

    Fixed pepega-api

--- apps/pepega-api/src/app/providers/pubsub.provider.ts
+++ apps/pepega-api/src/app/providers/pubsub.provider.ts
@@ -7,7 +7,11 @@ export const pubSubFactory = {
   inject: [ConfigService],
   useFactory: (configService: ConfigService) => {
     return new RedisPubSub({
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
       publisher: new Redis(configService.get('db.redisUrl')),
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
       subscriber: new Redis(configService.get('db.redisUrl')),
     });
   },

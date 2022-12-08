5de69b67af2bc34447299e6af66b9a7f04fd2813
Anton Savitskiy <sygeman@gmail.com>
2022-12-09 00:18:22 +0300

    Vahui with interval

--- apps/dream/src/pages/index.tsx
+++ apps/dream/src/pages/index.tsx
@@ -1,14 +1,24 @@
-import { createSignal } from 'solid-js';
+import { createEffect, createSignal } from 'solid-js';
 
 const IndexPage = () => {
   const [fullVahui, setFullVahiu] = createSignal(false);
+
+  createEffect(() => {
+    const interval = setInterval(() => {
+      setFullVahiu((fullVahui) => !fullVahui);
+    }, 2e3);
+
+    return () => clearInterval(interval);
+  });
 
   return (
     <div class="text-white insert-0 flex justify-center items-center h-screen w-screen">
       <img
         src="/vahui.webp"
-        onClick={() => setFullVahiu((fullVahui) => !fullVahui)}
-        classList={{ 'rounded-lg': true, 'scale-[5]': fullVahui() }}
+        classList={{
+          'rounded-lg transition-transform': true,
+          'scale-[5]': fullVahui(),
+        }}
       />
     </div>
   );

1c87fd9c509e1d41daee57b1a30d04d9ade7325f
Anton Savitskiy <sygeman@gmail.com>
2022-12-09 00:11:46 +0300

    Scale vahui

--- apps/dream/src/pages/index.tsx
+++ apps/dream/src/pages/index.tsx
@@ -1,7 +1,15 @@
+import { createSignal } from 'solid-js';
+
 const IndexPage = () => {
+  const [fullVahui, setFullVahiu] = createSignal(false);
+
   return (
     <div class="text-white insert-0 flex justify-center items-center h-screen w-screen">
-      <img src="/vahui.webp" class="rounded-lg" />
+      <img
+        src="/vahui.webp"
+        onClick={() => setFullVahiu((fullVahui) => !fullVahui)}
+        classList={{ 'rounded-lg': true, 'scale-[5]': fullVahui() }}
+      />
     </div>
   );
 };

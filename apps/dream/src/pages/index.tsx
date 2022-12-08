343bd6fb6b74f90894bb5d7e412683a4aa5126e1
Anton Savitskiy <sygeman@gmail.com>
2022-12-09 00:37:31 +0300

    Vahui improvments

--- apps/dream/src/pages/index.tsx
+++ apps/dream/src/pages/index.tsx
@@ -12,14 +12,21 @@ const IndexPage = () => {
   });
 
   return (
-    <div class="text-white insert-0 flex justify-center items-center h-screen w-screen">
-      <img
-        src="/vahui.webp"
+    <div class="text-white insert-0 flex justify-center items-center h-screen w-screen transition-transform">
+      <div
         classList={{
-          'rounded-lg transition-transform': true,
-          'scale-[5]': fullVahui(),
+          'animate-spin': fullVahui(),
         }}
-      />
+      >
+        <img
+          src="/vahui.webp"
+          classList={{
+            'scale-[5]': fullVahui(),
+            'rounded-lg': !fullVahui(),
+            'rounded-full': fullVahui(),
+          }}
+        />
+      </div>
     </div>
   );
 };

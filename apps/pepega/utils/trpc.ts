df23d8561a6b2100356d1e8ebb4f01426d11bed1
Sygeman <sygeman@gmail.com>
2022-12-18 02:25:37 +0300

    Removed emoji controller

--- apps/pepega/utils/trpc.ts
+++ apps/pepega/utils/trpc.ts
@@ -3,16 +3,7 @@ import { createTRPCNext } from '@trpc/next';
 import type { AppRouter } from '../server/routers/_app';
 
 function getBaseUrl() {
-  if (typeof window !== 'undefined')
-    // browser should use relative path
-    return '';
-  if (process.env.VERCEL_URL)
-    // reference for vercel.com
-    return `https://${process.env.VERCEL_URL}`;
-  if (process.env.RENDER_INTERNAL_HOSTNAME)
-    // reference for render.com
-    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
-  // assume localhost
+  if (typeof window !== 'undefined') return '';
   return `http://localhost:${process.env.PORT ?? 4200}`;
 }
 export const trpc = createTRPCNext<AppRouter>({

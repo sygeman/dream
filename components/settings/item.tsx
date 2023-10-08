88706a018456d1cc323e03fec81255e911f14274
Sygeman <sygeman@gmail.com>
2023-10-08 16:38:35 +0300

    ItemLink upd

--- components/settings/item.tsx
+++ components/settings/item.tsx
@@ -1,8 +1,8 @@
-import clsx from 'clsx';
+import cn from 'clsx';
 import Link from 'next/link';
 import React from 'react';
 
-import { Button } from '../ui/button';
+import { Button } from '@/components/ui/button';
 
 type Properties = {
   title: string;
@@ -11,18 +11,13 @@ type Properties = {
 };
 
 export const ItemLink = ({ title, selected = false, link }: Properties) => (
-  <div>
-    <Link
-      href={link}
-      passHref
-      // className={clsx(
-      //   'btn w-full justify-start my-0.5',
-      //   selected ? 'bg-zinc-900 text-white' : 'text-muted-foreground'
-      // )}
+  <Link href={link} passHref>
+    <Button
+      variant="ghost"
+      size="sm"
+      className={cn('w-full justify-start', selected && 'bg-accent')}
     >
-      <Button variant="ghost" className="w-full justify-start">
-        {title}
-      </Button>
-    </Link>
-  </div>
+      {title}
+    </Button>
+  </Link>
 );

b93b00dce8ac6e2e5fb901b60aa8256f26b32fa0
Sygeman <sygeman@gmail.com>
2023-10-08 17:27:58 +0300

    fix channel settings button

--- app/[community]/[channel]/header.tsx
+++ app/[community]/[channel]/header.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link';
 import { usePathname, useSearchParams } from 'next/navigation';
 import React, { useMemo } from 'react';
 
+import { Button } from '@/components/ui/button';
+
 type Properties = {
   title?: string;
 };
@@ -23,15 +25,13 @@ export const ChannelHeader = ({ title }: Properties) => {
       <div className="h-full flex flex-1 items-center text-sm">{title}</div>
 
       <div className="h-full flex items-center ml-4">
-        <Link
-          className="h-8 w-8 rounded hover:bg-zinc-900-light flex items-center justify-center"
-          href={channelSettingsLink}
-          passHref
-        >
-          <CogIcon
-            className="w-4 h-4 text-muted-foreground"
-            aria-hidden="true"
-          />
+        <Link href={channelSettingsLink} passHref>
+          <Button variant="ghost" size="sm">
+            <CogIcon
+              className="w-4 h-4 text-muted-foreground"
+              aria-hidden="true"
+            />
+          </Button>
         </Link>
       </div>
     </div>

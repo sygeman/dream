5d1e53d96d893170a9ac2e9a35e88796b29cb84f
Sygeman <sygeman@gmail.com>
2025-04-03 18:12:17 +0300

    refactor: упрощение навигации и структуры страниц

---
+++ components/ui/header.tsx
@@ -0,0 +1,26 @@
+import { Logo } from "@/components/ui/logo";
+import { ThemeToggle } from "@/components/ui/theme-toggle";
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <div className="container mx-auto px-4">
+      <div className="flex items-center justify-between py-8">
+        <div className="flex items-center gap-8">
+          <Link href="/">
+            <Logo withBackground />
+          </Link>
+          <Link
+            href="/games"
+            className="text-black/60 dark:text-white/60 hover:text-black dark:hover:text-white transition-colors duration-300"
+          >
+            Игры
+          </Link>
+        </div>
+        <div className="relative">
+          <ThemeToggle />
+        </div>
+      </div>
+    </div>
+  );
+}

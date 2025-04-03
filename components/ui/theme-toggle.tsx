beb9c3a71ef1d5235cf872347ada521284d81e5e
Sygeman <sygeman@gmail.com>
2025-04-04 00:01:16 +0300

    fix: упрощение реализации кнопки переключения темы

--- components/ui/theme-toggle.tsx
+++ components/ui/theme-toggle.tsx
@@ -17,12 +17,13 @@ export function ThemeToggle() {
   return (
     <button
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      className="text-black/60 dark:text-white/60 hover:text-black dark:hover:text-white transition-colors duration-300 relative"
+      className="text-black/60 dark:text-white/60 hover:text-black dark:hover:text-white transition-colors duration-300"
     >
-      <div className="relative size-5">
-        <SunIcon className="absolute inset-0 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        <MoonIcon className="absolute inset-0 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      </div>
+      {theme === "dark" ? (
+        <SunIcon className="size-5" />
+      ) : (
+        <MoonIcon className="size-5" />
+      )}
       <span className="sr-only">Переключить тему</span>
     </button>
   );
